859e87aaeca9618f9911506ab89f724e952841de
Thiago <xfatalgames@gmail.com>
2022-09-29 16:04:12 -0300

    Descobrindo o menor numero

--- Exercícios/Fundamentos/secao-04-introdução-à-javascript/dia-02-array-e-loop-for/Exercicios.js
+++ Exercícios/Fundamentos/secao-04-introdução-à-javascript/dia-02-array-e-loop-for/Exercicios.js
@@ -70,3 +70,18 @@ if (result3 === 0) {
 } else {
   console.log(result);
 }
+
+
+let number7 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// o valor da variável smallestNumber poderia ser qualquer um, desde que fosse maior que o maior número do array numbers. Caso atribuíssemos o valor 1 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número menor que 1 no array. Para resolver esse problema vamos iniciar a variável com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+
+let smallestNumber = numbers7[0];
+
+for (let index = 1; index < numbers7.length; index += 1) {
+  if (numbers7[index] < smallestNumber) {
+    smallestNumber = numbers7[index];
+  }
+}
+
+console.log(smallestNumber);
