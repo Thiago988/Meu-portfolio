ea98152d106f3a775c49b0b16bd2aa50726c7eba
Thiago <xfatalgames@gmail.com>
2022-09-29 15:50:42 -0300

    Descobrindo o maior numero dentro de um array

--- Exercícios/Fundamentos/secao-04-introdução-à-javascript/dia-02-array-e-loop-for/Exercicios.js
+++ Exercícios/Fundamentos/secao-04-introdução-à-javascript/dia-02-array-e-loop-for/Exercicios.js
@@ -39,3 +39,18 @@ if (result2 > 20) {
 } else {
   console.log('valor menor ou igual a 20');
 }
+
+
+let numbers5 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema, vamos iniciar a variável com o primeiro valor do array. Dessa forma, podemos pular a posição 0 e iniciar com index = 1
+
+let higherNumber = numbers5[0];
+
+for (let index = 1; index < numbers5.length; index += 1) {
+  if (numbers5[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
+}
+
+console.log(higherNumber);
