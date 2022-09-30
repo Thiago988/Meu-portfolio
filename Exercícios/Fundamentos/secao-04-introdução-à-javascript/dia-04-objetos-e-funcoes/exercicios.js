6f2d86dc3de411d478d3b181b22b03598e1016e6
Thiago <xfatalgames@gmail.com>
2022-09-30 15:17:56 -0300

    percorrendo o objeto e imprimindo

--- Exercícios/Fundamentos/secao-04-introdução-à-javascript/dia-04-objetos-e-funcoes/exercicios.js
+++ Exercícios/Fundamentos/secao-04-introdução-à-javascript/dia-04-objetos-e-funcoes/exercicios.js
@@ -5,3 +5,14 @@ let info = {
   };
 
   console.log('Bem-vinda, '+ info.personagem);
+
+  let info2 = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  };
+  
+  for(let properties in info) {
+    console.log(info[properties]);
+  }
