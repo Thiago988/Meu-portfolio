49c486273a275202f269a5b3c82b3051bfef87d7
Thiago <xfatalgames@gmail.com>
2022-09-30 15:01:32 -0300

    descobrindo a menor e maior palavra

--- Exercícios/Fundamentos/secao-04-introdução-à-javascript/dia-03-lógica-de-programacao-e-algoritmos/exercicios.js
+++ Exercícios/Fundamentos/secao-04-introdução-à-javascript/dia-03-lógica-de-programacao-e-algoritmos/exercicios.js
@@ -17,3 +17,24 @@ for (let index = 0; index < word.length; index += 1) {
 }
 
 console.log(reverseWord);
+
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord) ; 
